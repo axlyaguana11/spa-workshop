cfddc66aa9b89e5d9d877ef740d80905af964efb
axl <axl9809@gmail.com>
2021-03-18 17:59:17 -0500

    Routes.js created

---
+++ routes.js
@@ -0,0 +1,14 @@
+const routes = [
+    {
+        path: '/',
+        template: '<h1>Hello</h1>'
+    },
+    {
+        path: '/contact',
+        template: '<h1>Contact</h1>'
+    },
+    {
+        path: '/aboutme',
+        template: '<h1>About Me</h1>'
+    },
+]
