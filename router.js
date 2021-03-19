9793086a2aed3c0c53abde8799ea809976092e6b
axl <axl9809@gmail.com>
2021-03-18 23:43:45 -0500

    loadInitialRoute 2nd V

--- router.js
+++ router.js
@@ -3,11 +3,29 @@ class Router {
         this.routes = routes
         this._loadInitialRoute()
     }
+
+_matchUrlToRoute(urlSegs) {
+    const matchedRoute = this.routes.find( route => {
+        const routePathSegs = route.path.split('/').slice(1)
+
+        if(routePathSegs.length !== urlSegs.length){
+            return false
+        }
+
+        return routePathSegs
+            .every((routePathSeg, i) => routePathSeg === urlSegs[i])
+    })
+
+    return matchedRoute
 }
+
 
 _loadInitialRoute() {
     const pathNameSplit = window.location.pathname.split('/') 
     const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : ''
 
     this.loadRoute(...pathSegs)
+    }
+
 }
+
